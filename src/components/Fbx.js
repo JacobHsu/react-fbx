c9ebd90dcdbe161bf1f30e1547c0cf1f9c7bbb28
jacobhsu <jacob.hsu.tw@gmail.com>
2023-04-07 14:42:06 +0800

    feat: Scene2

--- src/components/Fbx.js
+++ src/components/Fbx.js
@@ -10,15 +10,36 @@ import { Suspense } from 'react';
 const Scene = () => {
 
   const url = 'https://threejs.org/examples/models/fbx/Samba%20Dancing.fbx';
-  // const url2 ='https://github.com/mrdoob/three.js/raw/master/examples/models/fbx/Samba%20Dancing.fbx';
+  const url2 ='https://github.com/mrdoob/three.js/raw/master/examples/models/fbx/Samba%20Dancing.fbx';
+  const url3 = 'https://hopeseedblob.blob.core.windows.net/nft-jini/41_0xfe2a01997821dfbebd1b9352d44190e96e9a1b26_68_1680794274.fbx';
+  const url4 = 'https://parallelworld.blob.core.windows.net/coin/41_0xfe2a01997821dfbebd1b9352d44190e96e9a1b26_68_1680794274.fbx';
+  const url5 = 'https://parallelworld.blob.core.windows.net/coin/Samba%20Dancing.fbx';
+  const url6 = 'https://parallelworld.blob.core.windows.net/coin/Dragon_Baked_Actions_fbx_6.1_ASCII.fbx';
+  const url7 = 'https://parallelworld.blob.core.windows.net/coin/Dragon%202.5_fbx.fbx';
   // const fbx = useFBX(url); //  gun.fbx // 'test.fbx'
   const file = 'test.fbx';
+  // const fbx = useLoader(FBXLoader, file)
+  const fbx = useLoader(FBXLoader, url7);
+
+  return (
+    <>
+      <ambientLight intensity={0.05} />
+      <pointLight position={[10, 10, 10]} />
+      <primitive object={fbx} scale={0.1} position={[0, -2, 0]} />
+    </>
+  );
+};
+
+const Scene2 = () => {
+
+  const url = 'https://threejs.org/examples/models/fbx/Samba%20Dancing.fbx';
+
   // const fbx = useLoader(FBXLoader, file)
   const fbx = useLoader(FBXLoader, url);
 
   return (
     <>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.05} />
       <pointLight position={[10, 10, 10]} />
       <primitive object={fbx} scale={0.03} position={[0, -3, 0]} />
     </>
@@ -33,6 +54,13 @@ const Fbx = () => {
           <Scene />
           <OrbitControls />
           <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Scene2 />
+          <OrbitControls />
+          {/* <Environment preset="sunset" background /> */}
         </Suspense>
       </Canvas>
     </div>
